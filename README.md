60b8bd08c123bd37b2cf81d9fe3cc4437c7da80d
No767 <73260931+No767@users.noreply.github.com>
2022-10-10 12:16:27 -0700

    Upload Files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kumiko Unloaded Cogs
+
+These are unloaded cogs that Kumiko once used. 
+
+## License
+
+Apache-2.0
---
+++ Unloaded-Cogs/ban.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+
+class BanV1(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Use this with caution...
+    @commands.is_owner()
+    @commands.command(name="ban")
+    async def banUser(self, ctx):
+        await ctx.send(discord.Permissions.ban_members())
+
+
+def setup(bot):
+    bot.add_cog(BanV1(bot))
